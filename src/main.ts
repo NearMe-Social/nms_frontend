2c53b534f18de9729479f14c3052ad82e83fad1f
SokhomPanha <sokhompanha70@gmail.com>
2026-06-03 17:09:29 +0700

    odom: integrate real map with openstreetview by leaflet and fix the nearby user problem

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'leaflet/dist/leaflet.css'
 
 export const pinia = createPinia()   
 
